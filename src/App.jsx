560e5e23c78d5d982480dd80af03f6ad978c18e0
Abhiram-Vakil <abhiramvakil@gmail.com>
2025-02-17 09:43:38 +0530

    Added header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Navbar from './components/Navbar/Navbar';
+import Stall from './pages/Stall/Stall'
 import './App.css'
 
 function App() {
@@ -11,9 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/stall" element={<Stall />} />
       </Routes>
       <Navbar className='mindaa'></Navbar>
-
     </Router>
   )
 }
--- src/pages/Stall/Stall.jsx
+++ src/pages/Stall/Stall.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
 
 function Stall() {
   return (
-    <div>Stall</div>
+    
+    <div>
+      <Header/>
+      Stall
+    </div>
   )
 }
 
