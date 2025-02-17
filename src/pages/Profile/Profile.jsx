626304697165ea21c22506c36e5344e63b1ace9b
Abhiram-Vakil <abhiramvakil@gmail.com>
2025-02-17 11:48:27 +0530

    updated profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,14 +2,29 @@ import React from 'react'
 import './profile.css'
 import Header from '../../components/Header/Header'
 import Titles from '../../components/Titles/Titles'
-
+import profileicon from '../../assets/profileicon.svg'
+import profiledp from '../../assets/profile.png'
 function Profile() {
   return (
     <div>
         <Header/>
         <Titles title="Profile" sub="It’s all about you !"/>
-
-      Profile
+        <div className='profilesection'>
+          <img src={profiledp} alt=""  />
+          <h1>Team Name</h1>
+          <h2>Project Name</h2>
+        </div>
+        <h1 className='member'>Members</h1>
+        <div className='memberssection'>
+          <div className='sec1'>
+            <div className='indmember'> <img src={profileicon} alt=""/><p>Alan Jose Santo</p></div>
+            <div className='indmember'> <img src={profileicon} alt="" /><p>Alan Jose Santo</p></div>
+          </div>
+          <div className='sec2'>
+            <div className='indmember'> <img src={profileicon} alt="" /><p>Alan Jose Santo</p></div>
+            <div className='indmember'> <img src={profileicon} alt="" /><p>Alan Jose Santo</p></div>
+          </div>
+        </div>
     </div>
   )
 }
