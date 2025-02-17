71ec0bcb9900dc4cfa94a26ad2b7430ecddcc668
Alan Jose Santo <realityaudiostudio@gmail.com>
2025-02-17 21:25:26 +0530

    add swiper component and styles; update navbar and app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Navbar from './components/Navbar/Navbar';
-import Stall from './pages/Stall/Stall'
-import Profile from './pages/Profile/Profile'
+import Stall from './pages/Stall/Stall';
+import Profile from './pages/Profile/Profile';
 import Project from './pages/Project/Project';
+import StallOpen from './components/StallOpen/StallOpen';
 
-function App() {
+function AppContent() {
+  const location = useLocation();
   return (
-    <Router>
+    <>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/stall" element={<Stall />} />
-        <Route path="/profile" element={<Profile />} />  
-        <Route path="/project" element={<Project />} />  
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/stallopen" element={<StallOpen />} />
       </Routes>
-      <Navbar className='mindaa'></Navbar>
-    </Router>
-  )
+      {location.pathname !== "/login" && <Navbar className='mindaa' />}
+
+    </>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+export default App;
