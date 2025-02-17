1db4512705eb04d886919b01dd45e6c509e8cb17
Abhiram-Vakil <abhiramvakil@gmail.com>
2025-02-17 13:52:23 +0530

    updated project page

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -5,28 +5,6 @@ import Titles from '../../components/Titles/Titles'
 import Section from '../../components/Section/Section'
 
 function Project() {
-  const data = [
-    {
-      head: "Breaking News",
-      para: "A major event has just happened in the city, causing widespread discussions.",
-      time: "10 mins ago"
-    },
-    {
-      head: "Tech Update",
-      para: "The latest smartphone model has been launched with groundbreaking features.",
-      time: "1 hour ago"
-    },
-    {
-      head: "Sports Highlights",
-      para: "The national team secured a thrilling victory in the finals last night.",
-      time: "3 hours ago"
-    },
-    {
-      head: "Weather Alert",
-      para: "Heavy rainfall expected throughout the weekend. Stay safe and take precautions.",
-      time: "5 hours ago"
-    }
-  ];
   
   const sectionData = [
     {
@@ -55,6 +33,7 @@ function Project() {
     <div>
         <Header/>
         <Titles title={"Project Title"} sub={"Team Name"}></Titles>
+        <button className='addbtn'>Add Update</button>
         <div className='sections'>
           {sectionData.map((item, index) => (
         <Section key={index} stage={item.stage} text={item.text} time={item.time} />
