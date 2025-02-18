dedaad6e9cef98a3f9f59caa02d15928331ac76f
Abhiram-Vakil <abhiramvakil@gmail.com>
2025-02-18 10:31:14 +0530

    updated stall page

--- src/pages/Stall/Stall.jsx
+++ src/pages/Stall/Stall.jsx
@@ -8,31 +8,46 @@ import { EffectCards } from 'swiper/modules';
 import './stall.css';
 import Frutty from '../../../public/images/packet-jiuce.png';
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 
 function Stall() {
-  return (
-    
-    <div>
+  const slideData = [
+    { name: "Frutty",price:"Rs.52", img: Frutty, link: "/stallopen" },
+    { name: "Maza",price:"Rs.10", img: Frutty, link: "/stallopen" },
+    { name: "shake",price:"Rs.80", img: Frutty, link: "/stallopen" },
+    { name: "boost",price:"Rs.82", img: Frutty, link: "/stallopen" },
+   
+  ];
+  
+  const [activeIndex, setActiveIndex] = useState(0);
 
+  
+  return (
+   
+    <div>
       <Header/>
       <Titles title="The Shop" sub="Need a break ! Call us we will be here !"/>
-      <Swiper
+      <div className='kada'>
+        <Swiper
         effect={'cards'}
         grabCursor={true}
         modules={[EffectCards]}
         className="mySwiper"
+        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
       >
-        <SwiperSlide><Link to='/stallopen'><img src={Frutty} alt='frutty'/></Link>
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {slideData.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <Link to={slide.link}>
+              <img src={slide.img} alt={slide.name} />
+            </Link>
+          </SwiperSlide>
+        ))}
       </Swiper>
+      <p className='stallitemname'>{slideData[activeIndex].name}</p>
+      <p className="stallitemprice">{slideData[activeIndex].price}</p>
+      <button className='stallbtn'>Order Now</button>
+      </div>
+      
     </div>
   )
 }
